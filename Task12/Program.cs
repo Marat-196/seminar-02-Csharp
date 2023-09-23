1cd21b7f1dcf3f517068e9481c54975c1f17f382
Marat <zhumataev.marat@bk.ru>
2023-09-23 14:17:36 +0500

    add Task 12,14,16

---
+++ Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿// 12. Напишите программу, которая будет принимать на
+// вход два числа и выводить, является ли первое число
+// кратным второму. Если первое число не кратно
+// второму, то программа выводит остаток от деления.
+// 34, 5 -> не кратно, остаток 4
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int MultipleNumber(int fDigit, int sDigit)
+{
+    return fDigit % sDigit;
+}
+
+int remainderNumber = MultipleNumber(firstNumber, secondNumber);
+
+if (remainderNumber == 0) Console.WriteLine($"{firstNumber}, {secondNumber} -> кратно");
+else Console.WriteLine($"{firstNumber}, {secondNumber} -> не кратно, остаток {remainderNumber}");
---
+++ Task16/Program.cs
@@ -0,0 +1,21 @@
+﻿// 16. Напишите программу, которая принимает на
+// вход два числа и проверяет, является ли одно
+// число квадратом другого.
+// 5, 25 -> да
+// -4, 16 -> да
+// 25, 5 -> да
+// 8,9 -> нет
+
+bool IsSquareTwoNumbers(int num1, int num2)
+{
+    return num1 * num1 == num2 || num2 * num2 == num1;
+}
+
+Console.Write("Введите первое число: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+bool result = IsSquareTwoNumbers(firstNum, secondNum);
+Console.WriteLine(result ? $"{firstNum}, {secondNum} -> Да" : $"{firstNum}, {secondNum} -> Нет");
